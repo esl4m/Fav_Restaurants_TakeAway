b486448b5132686e397e3e19060b2b649b4e77fd
Eslam Elgamal <me@esl4m.com>
2021-01-02 13:54:07 +0300

    Adds resturants utils func: List & Sort & Filter

--- utils/ResturantsSDK.js
+++ utils/ResturantsSDK.js
@@ -7,7 +7,7 @@ class ResturantsSDK {
     constructor(resturants) {
         this.resturants = resturants;
         this.result = resturants;
-
+        this.sort("bestMatch");  // Load the default sort: sort by "bestMatch"
         if (typeof window === "undefined") return this;
         if (!localStorage.favorites) localStorage.setItem("favorites", "");
         this.favorites = localStorage.favorites.split(",");
@@ -35,6 +35,80 @@ class ResturantsSDK {
         return this;
     }
 
+    /**
+     * List all resturants
+     */
+    list({ keyword, sortBy }) {
+        this.filter(keyword).sort(sortBy || "bestMatch");
+        const { favorites, normal } = this.result.reduce(
+            (accumulator, resturant) => {
+                if (this.favorites.includes(resturant.name)) {
+                resturant.favorite = true;
+                accumulator.favorites.push(resturant);
+                return accumulator;
+                }
+                resturant.favorite = false;
+                accumulator.normal.push(resturant);
+                return accumulator;
+            },
+            { favorites: [], normal: [] }
+        );
+        return favorites.concat(normal);
+    }
+
+    /**
+     * Sort resturants by keyword
+     */
+    sort(sortBy) {
+        // -- bestmatch by default (descending) -- //
+        const asc = [
+            "newest",
+            "distance",
+            "averageProductPrice",
+            "deliveryCosts",
+            "minCost"
+        ];
+
+        // if (returned value > 0) then the position of a and b is swapped.
+        // if (returned value <=0) then there is no change in position.
+        this.result.sort((a, b) => {
+            if (a.status === "open" && b.status !== "open") {
+                return -1;
+            }
+
+            if (a.status !== "open" && b.status === "open") {
+                return 1;
+            }
+
+            if (a.status === b.status) {
+                if (asc.includes(sortBy)) {
+                    return a.sortingValues[sortBy] - b.sortingValues[sortBy];
+                }
+                return b.sortingValues[sortBy] - a.sortingValues[sortBy];
+            }
+
+            if (a.status === "order ahead" && b.status !== "order ahead") {
+                return -1;
+            }
+            // else
+            return 1;
+        });
+        return this;
+    }
+
+    /**
+     * Filter resturants by keyword
+     */
+    filter(keyword) {
+        if (!keyword) {
+            this.result = this.resturants;
+            return this;
+        }
+        this.result = this.resturants.filter(({ name }) =>
+            name.toLowerCase().includes(keyword.toLowerCase())
+        );
+        return this;
+    }
 }
 
 export default ResturantsSDK;
